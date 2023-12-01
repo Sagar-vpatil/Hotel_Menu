78e5cc73cd2e37090b13cde9cde17d2fa477a4c1
Pratham9325 <prathammistar@gmail.com>
2023-12-01 12:25:59 +0530

    add new

--- Menu_Card/Menu_Card/urls.py
+++ Menu_Card/Menu_Card/urls.py
@@ -21,4 +21,6 @@ from myapp import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',views.index,name='home'),
+    path('add_menu',views.add_menu),
+      
 ]
--- Menu_Card/myapp/views.py
+++ Menu_Card/myapp/views.py
@@ -3,5 +3,7 @@ from Menu_Card import urls
 
 def index(request):
     return render(request,"index.html")
+def add_menu(request):
+    return render(request,"add_menu.html")
 
 # Create your views here.
